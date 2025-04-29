21465165afcd29b16c1914b8c0491c49eb477a3c
MihaiS-git <ingsuciumihai@gmail.com>
2025-04-29 18:25:32 +0300

    add base project

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header className='bg-gray-800 text-fuchsia-600 text-2xl font-bold px-8 py-4'>
+        <h1>Movie Shop</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ server/src/main/java/com/movieshop/server/ServerApplication.java
@@ -0,0 +1,15 @@
+package com.movieshop.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
