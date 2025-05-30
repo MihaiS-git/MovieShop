f0b4b79fc1118914ebef795be85175bc52805b14
MihaiS-git <ingsuciumihai@gmail.com>
2025-05-30 10:47:02 +0300

    change fonts

--- client/src/components/Root.tsx
+++ client/src/components/Root.tsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 const RootLayout = () => {
     
     return (
-        <main className="flex flex-col min-h-screen font-serif overflow-y-auto bg-light-background dark:bg-dark-background text-light-text dark:bg-dark-text">
+        <main className="flex flex-col min-h-screen font-display overflow-y-auto bg-light-background dark:bg-dark-background text-light-text dark:bg-dark-text">
             <div className="min-h-screen w-full bg-contain">
                 <Header />
                 <Outlet />
